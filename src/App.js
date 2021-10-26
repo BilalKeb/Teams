a7b13fe73c76b96b4874263f1a935bd5686e38af
BilalKeb <90609831+BilalKeb@users.noreply.github.com>
2021-10-26 21:35:55 +0200

    Teams

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <div className="container">
+      <h1>Teams</h1>
+      <h1 className="text-center">Teams</h1>
+      <div className="row">
+        {teams.map(element => <Team team={element} />)}
+      </div>
